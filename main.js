4acc80509719206eec4d120772b6fcf235ec43cd
Ovett Mora <ovettmora17@gmail.com>
2022-12-22 15:07:31 -0600

    cambio a ingles

--- main.js
+++ main.js
@@ -1,29 +1,25 @@
-
 const btnSubmit = document.querySelector('.botons');
-
 btnSubmit.addEventListener('click', saveData);
 
 function saveData() {
-    console.log("everything is ok");
     let firstName = document.querySelector('#name').value;
     let lastName = document.querySelector('#lastName').value;
     let age = document.querySelector('#age').value;
-    let maleSex = document.querySelector('#male').value;
-    let femaleSex = document.querySelector('#female').value;
+    let sexo= document.querySelector("input[name='sexo']:checked").value;
     let graduated = document.querySelector('#graduated').value;
     let email = document.querySelector('#email').value;
     let password = document.querySelector('#password').value;
 
-    let data = [];          
-    data.push = {
+    let data = {
         name: firstName,
         lastName: lastName,
         age: age,
-        sex: maleSex,
+        sex: sexo,
         graduated: graduated,
         email: email,
         password: password
     }
+
     console.log(data);
     document.getElementById("form-register").reset();   
 }
